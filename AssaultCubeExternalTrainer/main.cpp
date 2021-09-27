cd1c9771993e194fd8eceb6c69542c0e6f31d375
gohanko <26451183+gohanko@users.noreply.github.com>
2021-09-27 19:42:44 +0800

    Update

--- AssaultCubeExternalTrainer/main.cpp
+++ AssaultCubeExternalTrainer/main.cpp
@@ -32,7 +32,6 @@ uintptr_t GetModuleBaseAddress(DWORD processID, const wchar_t* moduleName) {
 		moduleEntry.dwSize = sizeof(moduleEntry);
 		if (Module32First(hSnapshot, &moduleEntry)) {
 			do {
-				std::cout << moduleEntry.szModule << std::endl;
 				if (!_wcsicmp(moduleEntry.szModule, moduleName)) {
 					baseAddress = (uintptr_t)moduleEntry.modBaseAddr;
 				}
@@ -54,15 +53,93 @@ uintptr_t FindDynamicMemoryAddress(HANDLE hProcess, uintptr_t baseAddress, std::
 	return address;
 }
 
+void PatchProcessMemory(HANDLE hProcess, BYTE* destination, BYTE* source, size_t size) {
+	DWORD oldMemoryProtectPermission;
+	VirtualProtectEx(hProcess, destination, size, PAGE_READWRITE, &oldMemoryProtectPermission);
+	WriteProcessMemory(hProcess, destination, source, size, nullptr);
+	VirtualProtectEx(hProcess, destination, size, oldMemoryProtectPermission, &oldMemoryProtectPermission);
+}
+
+void PatchProcessMemoryWithNoOP(HANDLE hProcess, BYTE* destination, size_t size) {
+	BYTE* noOPArray = new BYTE[size];
+	memset(noOPArray, 0x90, size);
+
+	PatchProcessMemory(hProcess, destination, noOPArray, size);
+	delete[] noOPArray;
+}
+
+void ClearConsole() {
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo(console, &screen);
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition(console, topLeft);
+}
+
 int main() {
 	DWORD processID = GetProcessID(L"ac_client.exe");
-	uintptr_t baseAddress = GetModuleBaseAddress(processID, L"ac_client.exe") + 0x10F4F4;
+	while (!processID) {
+		Sleep(1000);
+		ClearConsole();
+		std::cout << "AssaultCube process is not detected. Waiting..." << std::endl;
+		processID = GetProcessID(L"ac_client.exe");
+	}
+
+	std::cout << "AssaultCube process is detected. Activating cheat!" << std::endl;
+
+	bool bRecoil = false;
+	bool bAmmo = false;
+	bool bHealth = false;
+	int newValue = 1000;
+
+	uintptr_t moduleBase = GetModuleBaseAddress(processID, L"ac_client.exe");
 	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processID);
 
-	int newValue = 1000;
-	while (true) {
-		// Infinite Ammo
-		uintptr_t ammoAddress = FindDynamicMemoryAddress(hProcess, baseAddress, {0x374, 0x14, 0x0});
-		WriteProcessMemory(hProcess, (BYTE*)ammoAddress, &newValue, sizeof(newValue), nullptr);
+	DWORD terminationStatus = 0;
+	while (GetExitCodeProcess(hProcess, &terminationStatus) && terminationStatus == STILL_ACTIVE) {
+		if (GetAsyncKeyState(VK_NUMPAD1) & 1) {
+			bRecoil = !bRecoil;
+
+			if (bRecoil) {
+				PatchProcessMemoryWithNoOP(hProcess, (BYTE*)(moduleBase + 0x63786), 10);
+			}
+			else {
+				PatchProcessMemory(hProcess, (BYTE*)(moduleBase + 0x63786), (BYTE*)"\x50\x8D\x4C\x24\x1C\x51\x8B\xce\xff\xd2", 10);
+			}
+			std::cout << "No Recoil: " << std::boolalpha << bRecoil << std::endl;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD2) & 1) {
+			bHealth = !bHealth;
+			std::cout << "Infinite Health: " << std::boolalpha << bHealth << std::endl;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD3) & 1) {
+			bAmmo = !bAmmo;
+			if (bAmmo) {
+				PatchProcessMemory(hProcess, (BYTE*)(moduleBase + 0x637e9), (BYTE*)"\xFF\x06", 2);
+			}
+			else {
+				PatchProcessMemory(hProcess, (BYTE*)(moduleBase + 0x637e9), (BYTE*)"\xFF\x0E", 2);
+			}
+			std::cout << "Infinite Ammo: " << std::boolalpha << bAmmo << std::endl;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD4) & 1) {
+			return 0;
+		}
+
+		if (bHealth) {
+			uintptr_t healthAddress = FindDynamicMemoryAddress(hProcess, (moduleBase + 0x10F4F4), {0xF8});
+			WriteProcessMemory(hProcess, (BYTE*)healthAddress, &newValue, sizeof(newValue), nullptr);
+		}
+		
+		Sleep(500);
 	}
 }
